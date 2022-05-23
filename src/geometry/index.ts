60c2bf7cf1c5edc2bb1192e1cebdb4de92271c97
louisdutton <contact@louisdutton.com>
2022-05-23 13:23:47 +0100

    further intersection typing

--- src/geometry/index.ts
+++ src/geometry/index.ts
@@ -1,5 +1,13 @@
+import { Box } from "./classes/box";
+import { Line } from "./classes/line";
+import { Point } from "./classes/point";
+import { Segment } from "./classes/segment";
+
+export type Shape = Point | Line | Ray | Circle | Box | Segment | Arc | Polygon;
+export interface Renderable {
+  svg(): string;
+}
+
 export * as Constants from "./utils/constants";
 export * as Utils from "./utils/utils";
 export * as Errors from "./utils/errors";
-
-export type Shape = Point | Line | Ray | Circle | Box | Segment | Arc | Polygon;
